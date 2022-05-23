9b11b5145e0f85d9dfcd21cf57edaf4dc94adeb5
Ofer Zadikario <ofer.zadikario@gmail.com>
2022-05-23 08:42:03 +0300

    Add project files.

---
+++ ExampleRatingBar/ExampleRatingBar/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.WindowsSpecific;
+using Xamarin.Forms.Xaml;
+using Application = Xamarin.Forms.Application;
+
+namespace ExampleRatingBar
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Application.Current.On<Windows>().SetImageDirectory("Assets");
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ExampleRatingBar/ExampleRatingBar/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExampleRatingBar
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private int ratingVal;
+        public int RatingValue
+        {
+            get
+            {
+                return this.ratingVal;
+            }
+            set
+            {
+                this.ratingVal = value;
+                OnPropertyChanged("RatingValue");
+            }
+        }
+        public MainPage()
+        {
+            this.BindingContext = this;
+            InitializeComponent();
+            RatingValue = 0;
+        }
+    }
+}
